c36e40c077f4b1040adeca918d6ee37e8035373e
Harry <harry.b21@gmail.com>
2023-02-18 00:08:28 +0200

    Video 4 in Othello series

---
+++ Main 2/main2.py
@@ -0,0 +1,128 @@
+import pygame
+import random
+import copy
+
+#  utility functions
+def loadImages(path, size):
+    """Load an image into the game, and scale the image"""
+    img = pygame.image.load(f"{path}").convert_alpha()
+    img = pygame.transform.scale(img, size)
+    return img
+
+def loadSpriteSheet(sheet, row, col, newSize, size):
+    """creates an empty surface, loads a portion of the spritesheet onto the surface, then return that surface as img"""
+    image = pygame.Surface((32, 32)).convert_alpha()
+    image.blit(sheet, (0, 0), (row * size[0], col * size[1], size[0], size[1]))
+    image = pygame.transform.scale(image, newSize)
+    image.set_colorkey('Black')
+    return image
+
+#  Classes
+class Othello:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1100, 800))
+        pygame.display.set_caption('Othello')
+
+        self.rows = 8
+        self.columns = 8
+
+        self.grid = Grid(self.rows, self.columns, (80, 80), self)
+
+        self.RUN = True
+
+    def run(self):
+        while self.RUN == True:
+            self.input()
+            self.update()
+            self.draw()
+
+    def input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.RUN = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    self.grid.printGameLogicBoard()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        self.grid.drawGrid(self.screen)
+        pygame.display.update()
+
+class Grid:
+    def __init__(self, rows, columns, size, main):
+        self.GAME = main
+        self.y = rows
+        self.x = columns
+        self.size = size
+        self.whitetoken = loadImages('assets/WhiteToken.png', size)
+        self.blacktoken = loadImages('assets/BlackToken.png', size)
+        self.transitionWhiteToBlack = [loadImages(f'assets/BlackToWhite{i}.png', self.size) for i in range(1, 4)]
+        self.transitionBlackToWhite = [loadImages(f'assets/WhiteToBlack{i}.png', self.size) for i in range(1, 4)]
+        self.bg = self.loadBackGroundImages()
+
+        self.gridBg = self.createbgimg()
+
+        self.gridLogic = self.regenGrid(self.y, self.x)
+
+    def loadBackGroundImages(self):
+        alpha = 'ABCDEFGHI'
+        spriteSheet = pygame.image.load('assets/wood.png').convert_alpha()
+        imageDict = {}
+        for i in range(3):
+            for j in range(7):
+                imageDict[alpha[j]+str(i)] = loadSpriteSheet(spriteSheet, j, i, (self.size), (32, 32))
+        return imageDict
+
+    def createbgimg(self):
+        gridBg = [
+            ['C0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'E0'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'E2'],
+        ]
+        image = pygame.Surface((960, 960))
+        for j, row in enumerate(gridBg):
+            for i, img in enumerate(row):
+                image.blit(self.bg[img], (i * self.size[0], j * self.size[1]))
+        return image
+
+    def regenGrid(self, rows, columns):
+        """generate an empty grid for logic use"""
+        grid = []
+        for y in range(rows):
+            line = []
+            for x in range(columns):
+                line.append(0)
+            grid.append(line)
+
+        return grid
+
+    def drawGrid(self, window):
+        window.blit(self.gridBg, (0, 0))
+
+    def printGameLogicBoard(self):
+        print('  | A | B | C | D | E | F | G | H |')
+        for i, row in enumerate(self.gridLogic):
+            line = f'{i} |'.ljust(3, " ")
+            for item in row:
+                line += f"{item}".center(3, " ") + '|'
+            print(line)
+        print()
+
+
+if __name__ == '__main__':
+    game = Othello()
+    game.run()
+    pygame.quit()
